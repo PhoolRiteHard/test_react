243ae66141a1356d8812fdeb8d084ba88d473703
PhoolRiteHard <ilias.ryffel@gmail.com>
2023-03-28 17:19:16 +0200

    correction bug

--- my-new-app/src/App.jsx
+++ my-new-app/src/App.jsx
@@ -1,17 +1,22 @@
-function PokemonCard(props) {
-  let pokemon = props.pokemon;
+import  PokemonCard from "./components/PokemonCard"
+
+function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+ const pokemon = pokemonList[0];
 
   return (
-    <figure>
-      {pokemon.imgSrc ? (
-        <img src={pokemon.imgSrc} alt={pokemon.name} />
-      ) : (
-        <p>???</p>
-      )}
-      <figcaption>{pokemon.name}</figcaption>
-    </figure>
+    <div>
+      <PokemonCard pokemon= {pokemon}/>
+    </div>
   );
 }
 
-export default PokemonCard;
-
+export default App;
--- my-new-app/src/components/PokemonCard.jsx
+++ my-new-app/src/components/PokemonCard.jsx
@@ -1,37 +1,15 @@
+function PokemonCard (pokemon) {
 
-import React from "react";
-
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-function PokemonCard() {
-  const [pokemonIndex, setPokemonIndex] = React.useState(0);
-  const pokemon = pokemonList[pokemonIndex];
-
-  return (
-    <figure>
-      {pokemon.imgSrc ? (
-        <img src={pokemon.imgSrc} alt={pokemon.name} />
-      ) : (
-        <p>???</p>
-      )}
-      <figcaption>{pokemon.name}</figcaption>
-      <button onClick={() => setPokemonIndex((pokemonIndex + 1) % 2)}>
-        Next
-      </button>
-    </figure>
-  );
+return pokemon.pokemon.imgSrc !== undefined ?
+<figure>
+  <img src={pokemon.pokemon.imgSrc} alt={pokemon.pokemon.name} />
+  <figcaption>{pokemon.pokemon.name}</figcaption>
+</figure>
+:
+<figure>
+  <p>???</p>
+  <figcaption>{pokemon.pokemon.name}</figcaption>
+</figure>
 }
-
-
-
 
 export default PokemonCard;
