20835074be7baabfc1b9fd72cb5149c52ef57e11
PhoolRiteHard <ilias.ryffel@gmail.com>
2023-03-28 16:45:46 +0200

    je sais pas

--- my-new-app/src/components/PokemonCard.jsx
+++ my-new-app/src/components/PokemonCard.jsx
@@ -1,34 +1,18 @@
-
 import React from "react";
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+function PokemonCard(props) {
+const { pokemon } = props;
 
-function PokemonCard() {
-  const [pokemonIndex, setPokemonIndex] = React.useState(0);
-  const pokemon = pokemonList[pokemonIndex];
-
-  return (
-    <figure>
-      {pokemon.imgSrc ? (
-        <img src={pokemon.imgSrc} alt={pokemon.name} />
-      ) : (
-        <p>???</p>
-      )}
-      <figcaption>{pokemon.name}</figcaption>
-      <button onClick={() => setPokemonIndex((pokemonIndex + 1) % 2)}>
-        Next
-      </button>
-    </figure>
-  );
+return (
+<figure>
+{pokemon.imgSrc ? (
+<img src={pokemon.imgSrc} alt={pokemon.name} />
+) : (
+<p>???</p>
+)}
+<figcaption>{pokemon.name}</figcaption>
+</figure>
+);
 }
 
 export default PokemonCard;
