b5310a024966b724fb446bd665d36ad95208d65e
PhoolRiteHard <ilias.ryffel@gmail.com>
2023-03-29 10:33:03 +0200

    test q5

--- my-new-app/src/components/PokemonCard.jsx
+++ my-new-app/src/components/PokemonCard.jsx
@@ -1,6 +1,22 @@
+import PropTypes from 'prop-types';
+
 function PokemonCard(props) {
-  return props.imgSrc ? (<figure><img src={props.imgSrc} alt="Bulbosaur"/><figcaption>{props.name}</figcaption>
-  </figure>) : <p>???<figcaption>{props.name}</figcaption></p>;
+return props.imgSrc ? (
+<figure>
+<img src={props.imgSrc} alt={props.name} />
+<figcaption>{props.name}</figcaption>
+</figure>
+) : (
+<p>
+???
+<figcaption>{props.name}</figcaption>
+</p>
+);
 }
+
+PokemonCard.propTypes = {
+pokemon: PropTypes.shape({
+name: PropTypes.string.isRequired, imgSrc: PropTypes.string}).isRequired
+};
 
 export default PokemonCard;
